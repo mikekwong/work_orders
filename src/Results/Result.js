7333a2931638603e7bed4b1c402c555779eb66c1
Mike Wong <wongmdesign@gmail.com>
2020-01-15 22:20:57 -0500

    epoch time util func added

--- src/Results/Result.js
+++ src/Results/Result.js
@@ -5,11 +5,59 @@ const Result = ({
   worker,
   result: { deadline, description, name, workerId }
 }) => {
+  const convertDateTime = deadline => {
+    let a = new Date(deadline * 1000);
+    let months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec"
+    ];
+    let year = a.getFullYear();
+    let month = a.getMonth() + 1;
+    let date = a.getDate();
+    let hour = a.getHours();
+    let dd = "AM";
+    let h = hour;
+    if (hour >= 12) {
+      h = hour - 12;
+      dd = "PM";
+    }
+    if (hour === 0) {
+      h = 12;
+    }
+    let hourValue = "";
+    if (hour > 0 && hour <= 12) {
+      hourValue = "" + hour;
+    } else if (hour > 12) {
+      hourValue = "" + (hour - 12);
+    } else if (hour == 0) {
+      hourValue = "12";
+    }
+    let min =
+      a.getMinutes().toString().length === 1
+        ? `0${a.getMinutes()}`
+        : a.getMinutes();
+    let sec =
+      a.getSeconds().toString().length === 1
+        ? `0${a.getSeconds()}`
+        : a.getSeconds();
+    let time = `${month}/${date}/${year} ${hourValue}:${min}:${sec} ${dd}`;
+    return time;
+  };
   return (
     <>
       <ul>
         <li>{name}</li>
-        <li>{deadline}</li>
+        <li>{convertDateTime(deadline)}</li>
         <li>{description}</li>
         <li>{workerId}</li>
         {worker && (
